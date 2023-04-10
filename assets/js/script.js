449909259fcbf1ce0d29661ae8e58fa368fca5c9
Trollope91 <ljtdev91@gmail.com>
2023-04-10 11:41:43 +0000

    Add event listener for button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,46 @@
-function (runGame) {
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
 
 }
 
-function (checkAnswer) {
+function checkAnswer() {
 
 }
 
-function (calculateAnswer) {
+function calculateCorrectAnswer() {
 
 }
 
-function (incrementScore) {
+function incrementScore() {
 
 }
 
-function (incrementWrongAnswer) {
+function incrementWrongAnswer() {
 
 }
 
-function (displayAdditionQuestion) {
+function displayAdditionQuestion() {
 
 }
 
-function (dusplaySubtractQuestion) {
+function displaySubtractQuestion() {
 
 }
 
-function (displayMultiplyQuestion) {
+function displayMultiplyQuestion() {
     
 }
